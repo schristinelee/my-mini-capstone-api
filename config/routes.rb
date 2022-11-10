94070d51d00cbfd46d013745be37ea278a25c73b
schristinelee <christinelee1527@gmail.com>
2022-11-10 13:44:10 -0800

    adds request.http

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/products" => "products#index"
   get "/one_product/:id" => "products#show"
+
   get "/all_products" => "products#all_products"
   get "/first_product" => "products#first_product"
   get "/second_product" => "products#second_product"
