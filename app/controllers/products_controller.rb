c1d86a8d8e9428097d51f94b8ee0f5e747e0c30d
schristinelee <christinelee1527@gmail.com>
2022-11-10 01:46:28 -0800

    adds so many routes and actions for individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,19 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def first_product
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def second_product
+    product = Product.second
+    render json: product.as_json
+  end
+
+  def third_product
+    product = Product.third
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "/all_products" => "products#all_products"
+  get "/first_product" => "products#first_product"
+  get "/second_product" => "products#second_product"
+  get "/third_product" => "products#third_product"
 end
