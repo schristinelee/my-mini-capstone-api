291e4356fa914928ab2ac3789467003103df27cb
schristinelee <christinelee1527@gmail.com>
2022-11-10 01:20:57 -0800

    creates products using db/seeds.rb file with the rails db:seed command

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create({ name: "Apple Watch", price: 800, image_url: "https://www.google.com/search?q=apple+watch+series+8&tbm=isch&ved=2ahUKEwjbzq7ugJD7AhX9mI4IHbG7AmYQ2-cCegQIABAA&oq=apple+watch+series+8&gs_lcp=CgNpbWcQAzIHCAAQgAQQEzIHCAAQgAQQEzIHCAAQgAQQEzIHCAAQgAQQEzIHCAAQgAQQEzIHCAAQgAQQEzIHCAAQgAQQEzIICAAQBRAeEBMyCAgAEAUQHhATMggIABAFEB4QEzoECCMQJzoFCAAQgAQ6BAgAEB5Q2AZY3htgvx9oAHAAeACAAV-IAb8GkgECMTCYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=zqZiY9uvO_2xuvQPsfeKsAY&bih=393&biw=756#imgrc=yhu8-ae_thJmTM", description: "A wearable smartwatch that has a variety of functions." })
+
+Product.create({ name: "Macbook Pro", price: 2000, image_url: "https://www.google.com/search?q=macbook+pro&tbm=isch&ved=2ahUKEwjJ47qCgZD7AhWGoo4IHfiODoYQ2-cCegQIABAA&oq=macbook+pro&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BwgAEIAEEBM6CAgAEAcQHhATUPYTWO0fYPohaABwAHgAgAFhiAHOB5IBAjEymAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=-aZiY4mDB4bFuvQP-J26sAg&bih=393&biw=756#imgrc=wHwaeqr9lSTk3M", description: "A powerful laptop by 'Apple'." })
+
+Product.create({ name: "AirPods Pro", price: 250, image_url: "https://www.google.com/search?q=airpods+pro&tbm=isch&ved=2ahUKEwicpbHagpD7AhWsk44IHbJuCQYQ2-cCegQIABAA&oq=airpods+pro&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BAgjECdQmwhY5hRgyBtoAHAAeACAAVqIAc4HkgECMTKYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=vahiY9zSOaynuvQPst2lMA&bih=393&biw=756#imgrc=5EsyYujK8HLPkM", description: "Wireless bluetooth earbuds by Apple." })
