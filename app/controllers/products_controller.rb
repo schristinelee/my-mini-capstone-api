09684db55cb0b15ba766fa026b9f105a6630dee5
schristinelee <christinelee1527@gmail.com>
2022-11-10 19:56:08 -0800

    adds new requests to test the index, show, create, and update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,27 @@ class ProductsController < ApplicationController
     render json: product.as_json
   end
 
+  def create
+    product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+    )
+    product.save
+    render json: product.as_json
+  end
+
+  def update
+    product = Product.find_by(id: params[:id])
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    product.description = params[:description] || product.description
+    product.save
+    render json: product.as_json
+  end
+
   def all_products
     products = Product.all
     render json: products.as_json
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get "/products" => "products#index"
-  get "/one_product/:id" => "products#show"
-
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
   get "/all_products" => "products#all_products"
   get "/first_product" => "products#first_product"
   get "/second_product" => "products#second_product"
