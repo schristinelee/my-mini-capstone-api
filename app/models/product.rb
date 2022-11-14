54d92b24938a625af3c0532cbc465682863d55aa
schristinelee <christinelee1527@gmail.com>
2022-11-14 05:30:25 -0800

    adds supplier method to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,10 @@ class Product < ApplicationRecord
   # validates :description, presence: true
   # validates :description, length: { in: 10..500 }
 
+  def supplier
+    Supplier.find_by(id: supplier_id)
+  end
+
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-Product.create({ name: "Apple Watch", price: 800, image_url: "https://m.media-amazon.com/images/I/4120tn-nawL._AC_SY580_.jpg", description: "A wearable smartwatch that has a variety of functions." })
+# Supplier.create!(name: "Amazon", email: "amazon@email.com", phone_number: "23435234")
+# Supplier.create!(name: "Think Geek", email: "thinkgeek@email.com", phone_number: "3938393")
 
-Product.create({ name: "Macbook Pro", price: 2000, image_url: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6418/6418601_sd.jpg", description: "A powerful laptop by Apple." })
+# Product.create({ name: "Apple Watch", price: 800, image_url: "https://m.media-amazon.com/images/I/4120tn-nawL._AC_SY580_.jpg", description: "A wearable smartwatch that has a variety of functions." })
 
-Product.create({ name: "AirPods Pro", price: 250, image_url: "https://assets-prd.ignimgs.com/2022/09/07/airpods-pro-1662573080387.png", description: "Wireless bluetooth earbuds by Apple." })
+# Product.create({ name: "Macbook Pro", price: 2000, image_url: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6418/6418601_sd.jpg", description: "A powerful laptop by Apple." })
 
-Product.create({ name: "iMac", price: 1500, image_url: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6450/6450918_sd.jpg", description: "Desktop computer by Apple." })
+# Product.create({ name: "AirPods Pro", price: 250, image_url: "https://assets-prd.ignimgs.com/2022/09/07/airpods-pro-1662573080387.png", description: "Wireless bluetooth earbuds by Apple." })
 
-Product.create({ name: "AirPods Max", price: 600, image_url: "https://sm.mashable.com/t/mashable_sea/review/a/apple-airp/apple-airpods-max-review-if-you-cant-beats-em_uaq9.1248.jpg", description: "Wireless bluetooth headphones by Apple." })
+# Product.create({ name: "iMac", price: 1500, image_url: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6450/6450918_sd.jpg", description: "Desktop computer by Apple." })
+
+# Product.create({ name: "AirPods Max", price: 600, image_url: "https://sm.mashable.com/t/mashable_sea/review/a/apple-airp/apple-airpods-max-review-if-you-cant-beats-em_uaq9.1248.jpg", description: "Wireless bluetooth headphones by Apple." })
