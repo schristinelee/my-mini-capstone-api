6ad069fb3fd54522622bb7827ee26d172e092fb6
schristinelee <christinelee1527@gmail.com>
2022-11-14 02:51:46 -0800

    adds quantity to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_104224) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_105041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_104224) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
   end
 
 end
