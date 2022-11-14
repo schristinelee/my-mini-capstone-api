70e475f5fd9a4bdbb87bff574549914b76ad9ee1
schristinelee <christinelee1527@gmail.com>
2022-11-14 06:28:49 -0800

    cleans up code

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :images
+  belongs_to :supplier
 
   # validates :name, presence: true
   # validates :name, uniqueness: true
@@ -6,16 +8,6 @@ class Product < ApplicationRecord
   # validates :price, numericality: { greater_than: 0 }
   # validates :description, presence: true
   # validates :description, length: { in: 10..500 }
-
-  has_many :images
-  # def images
-  #   Image.where(product_id: id)
-  # end
-
-  belongs_to :supplier
-  # def supplier
-  #   Supplier.find_by(id: supplier_id)
-  # end
 
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,6 +1,3 @@
 class Supplier < ApplicationRecord
   has_many :products
-  # def products
-  #   Product.where(supplier_id: id)
-  # end
 end
