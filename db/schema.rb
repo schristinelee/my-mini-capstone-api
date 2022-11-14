3ff47f99126a4fca0749c7e7e55ce58628e875c8
schristinelee <christinelee1527@gmail.com>
2022-11-14 02:38:46 -0800

    changed the price to decimal and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_10_091144) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_103438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 9, scale: 2
     t.string "image_url"
     t.string "description"
     t.datetime "created_at", null: false
